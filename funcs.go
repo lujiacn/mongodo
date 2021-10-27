955375d6e147ae4df9082866648f8de34b7ace89
Jia Lu <lujiacn@gmail.com>
2021-10-27 18:32:13 +0800

    added createIndexSpecPartial

--- funcs.go
+++ funcs.go
@@ -11,6 +11,31 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// CreateIndexSpecPartial
+func CreateIndexSpecPartial(model interface{}, keys []string, unique bool, partialFilter bson.M) (string, error) {
+	colName := getModelName(model)
+	coll := MongoDB.Collection(colName)
+	indexView := coll.Indexes()
+	bKeys := bson.D{}
+	for _, k := range keys {
+		bKeys = append(bKeys, primitive.E{Key: k, Value: 1})
+	}
+
+	indexModel := mongo.IndexModel{
+		Keys: bKeys,
+		Options: &options.IndexOptions{
+			PartialFilterExpression: partialFilter,
+			Unique:                  &unique,
+		},
+	}
+
+	out, err := indexView.CreateOne(context.Background(), indexModel, options.CreateIndexes())
+	if err != nil {
+		return "", err
+	}
+	return out, nil
+}
+
 // All index with PartialFilterExperession IsRemoved = false
 // CreateIndex with keys and unique (true or false)
 func CreateIndex(model interface{}, keys []string, unique bool) (string, error) {
